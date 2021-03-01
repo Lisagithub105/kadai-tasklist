81a9027c03833a68c1a58621d4314bc183716634
lisa.techacademy <worldwideoceanstar105@gmail.com>
2021-03-01 01:33:18 +0000

    CRUD tasks

---
+++ resources/views/tasks/show.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1>タスク詳細ページ</h1>
+    <table class="table table-border">
+        <tr>
+            <th>id</th>
+            <td>{{ $task->id }}</td>
+        </tr>
+        <tr>
+            <th>タスク名</th>
+            <td>{{ $task->content }}</td>
+        </tr>
+    </table>
+    {{-- タスク名編集ページへリンク --}}
+    {!! link_to_route('tasks.edit', 'このタスク名を編集', ['task' => $task->id], ['class' => 'btn btn-light']) !!}
+    
+    {{-- タスク削除フォーム --}}
+    {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method'=>'delete']) !!}
+    {!! Form::submit('削除', ['class' => 'btn btn-danger']) !!}
+    {!! Form::close() !!}
+    
+    
+@endsection
