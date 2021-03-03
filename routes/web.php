1c4e0da0795180a4ec98e3c3b789dd3805671333
lisa.techacademy <worldwideoceanstar105@gmail.com>
2021-03-03 02:39:07 +0000

    user registration

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,8 @@
 
 Route::get('/', 'TasksController@index');
 
+// ユーザ登録
+Route::get('sigup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::get('signup', 'Auth\RegisterController@register')->name('signup.post');
+
 Route::resource('tasks', 'TasksController');
