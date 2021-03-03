a5634ed68f1226db7beb485926e3459048fef184
lisa.techacademy <worldwideoceanstar105@gmail.com>
2021-03-03 03:43:25 +0000

    user login and fix register

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,16 @@
 @extends('layouts')
 
 @section('content')
-    <div class="center jumbortron">
-        <div class="text-center">
-            <h1>Welcome to the Tasklist</h1>
-            {{-- ユーザ登録ページへのリンク --}}
-            {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary]') !!}
+
+    @if (Auth::check())
+    {{ Auth::user->name }}
+    @else
+        <div class="center jumbortron">
+            <div class="text-center">
+                <h1>Welcome to the Tasklist</h1>
+                {{-- ユーザ登録ページへのリンク --}}
+                {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary]') !!}
+            </div>
         </div>
-    </div>
+    @endif
 @endsection
