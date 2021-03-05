b1c6350e97c3be24dc0181806b4305f1180b77d4
lisa.techacademy <worldwideoceanstar105@gmail.com>
2021-03-05 09:57:05 +0000

    kadai1

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -16,16 +16,26 @@ class TasksController extends Controller
     // getでtasksにアクセスされた場合の「一覧表示処理」
     public function index()
     {
-        // tasks一覧を取得
-        $tasks = Task::all();
-        
-        // タスク一覧ビューでそれを表示
-        return view('tasks.index', [
-            'tasks' => $tasks,
-            ]);
-        
+        $data = [];
+        if (\Auth::check()) {
+            //認証済の場合
+            //認証済ユーザを取得
+            $user = \Auth::user();
+            //ユーザ投稿の一覧を作成日時の降順で取得
+            $tasks = $user->tasks()->orderBy('created_at', 'desc')->paginate(10);
+            $data = [
+                'user' => $user,
+                'tasks' => $tasks
+            ];
+            
+            // indexのviewに$tasksを渡す。
+            // return view('tasks.index', $tasks);
+            
+        }
+        // welcomeビューでそれらを表示
+        return view('welcome', $data);
     }
-
+        
     /**
      * Show the form for creating a new resource.
      *
@@ -56,6 +66,7 @@ class TasksController extends Controller
         
         // タスク名の作成
         $task = new Task;
+        $task->user_id = \Auth::id();
         $task->status = $request->status; // カラム追加
         $task->content = $request->content;
         $task->save();
@@ -75,11 +86,19 @@ class TasksController extends Controller
         // idの値でタスクを検索して取得
         $task = Task::findOrFail($id);
         
-        // タスク詳細ビューでそれを表示
-        return view('tasks.show', [
-            'task' => $task,
-            ]);
-    }
+        if (\Auth::id() === $task->user_id) {
+            // 自分自身のもの
+            // ユーザ詳細ビューでそれらを表示
+            return view('tasks.show', [
+                // 'user' => $user,
+                'task' => $task,
+        ]);
+        } 
+            // 自分以外のもの
+            // 前のURLへリダイレクトさせる
+            // welcomeビューでそれらを表示
+            return redirect('/');
+        }
 
     /**
      * Show the form for editing the specified resource.
@@ -92,11 +111,22 @@ class TasksController extends Controller
         // idの値でタスク名を検索して取得
         $task = Task::findOrFail($id);
         
-        // タスク名編集ビューでそれを表示
-        return view('tasks.edit', [
-            'task' => $task,
+        // 認証済ユーザ（閲覧者）がその投稿の所有者である場合は、投稿を削除
+        if (\Auth::id() === $task->user_id)
+        {
+            
+            // idの値でメッセージを検索して取得
+            $message = Message::findOrFail($id);
+            // タスク名編集ビューでそれを表示
+            return view('tasks.edit', [
+                'task' => $task,
             ]);
-    }
+            
+            
+        } 
+            // 前のURLへリダイレクトさせる
+            return back();    
+        }
 
     /**
      * Update the specified resource in storage.
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -2,31 +2,37 @@
 
 @section('content')
 
+<?php 
+//dd($tasks);
+?>
     <h1>タスク一覧</h1>
     
-    @if (count($tasks) > 0)
-    <table class="table table-striped">
-        <thead>
-            <tr>
-                <th>id</th>
-                <th>状況</th>
-                <th>タスク名</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($tasks as $task)
-            <tr>
-                {{-- メッセージ詳細ページへのリンク --}}
-                <td>{!! link_to_route('tasks.show', $task->id, ['task' => $task->id]) !!}</td>
-                <td>{{ $task->status }}</td>
-                <td>{{ $task->content }}</td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
-    @endif
+    @if (Auth::check())
+        @if (count($tasks) > 0)
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>id</th>
+                        <th>状況</th>
+                        <th>タスク名</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($tasks as $task)
+                    <tr>
+                        {{-- メッセージ詳細ページへのリンク --}}
+                        <td>{!! link_to_route('tasks.show', $task->id, ['task' => $task->id]) !!}</td>
+                        <td>{{ $task->status }}</td>
+                        <td>{{ $task->content }}</td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+            @endif
+            {{-- タスク作成ページへのリンク --}}
+            {!! link_to_route('tasks.create', '新規タスク名の投稿', [], ['class' => 'btn btn-primary']) !!}
     
-    {{-- タスク作成ページへのリンク --}}
-    {!! link_to_route('tasks.create', '新規タスク名の投稿', [], ['class' => 'btn btn-primary']) !!}
-
+    @else
+        まだログインしていません。
+    @endif
 @endsection
